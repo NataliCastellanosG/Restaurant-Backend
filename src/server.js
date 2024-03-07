6c03488ec657d440f5434dfb28487ab03ee1fbd3
NataliCastellanosG <nmcastellanos63145@umanizales.edu.co>
2024-03-07 14:23:50 -0500

    config product controller/router

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 1337;
 
 //routers
 const restaurantrouter = require('./Routers/restaurantrouter.js');
+const productrouter = require('./Routers/productrouter.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
@@ -24,3 +25,4 @@ connection.sync({force: false})
 
 //api
 app.use('/api', restaurantrouter);
+app.use('/api', productrouter);
